afd2282864af411835cb9b5cb84cfe171ac53569
G-Shadow <18046412+gshadows@users.noreply.github.com>
2022-08-23 22:54:11 +0300

    Set multiplication to 7 for better results.

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@
 if exist results del /F /Q results\*.*
 if not exist results mkdir results
 
-WeatherStats.exe --bg EmptyMap.jpg --mask Mask.jpg --mult 10 MeteoDiary results --log log.log
+WeatherStats.exe --bg EmptyMap.jpg --mask Mask.jpg --mult 7 MeteoDiary results --log log.log
